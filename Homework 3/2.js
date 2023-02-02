c80fa8e8a3c32e77d4c69fe8a3ebff1125b56a56
Chernakov Ilya <chernakov.ilya@gmail.com>
2023-02-02 19:20:09 +0600

    homework 3 done

---
+++ Homework 3/2.js
@@ -0,0 +1,13 @@
+// Cоздать функцию greeting, 
+// которая принимает имя и выводит приветствие, используя переданное имя, в консоль.
+// Необходимо у пользователя запросить имя и вызвать функцию greeting, 
+// передав туда данное значение.
+
+function greeting(userName){
+    alert(userName)
+}
+
+const nameUser = prompt('Enter your name: ');
+
+greeting(nameUser)
+//I have strange memories of this code... 
